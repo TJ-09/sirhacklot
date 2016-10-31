2dec9e1040624da28c74b012ca730e173a3a9fc3
TJ <timforman90@gmail.com>
2016-10-31 19:42:38 +0000

    cleanup

--- src/app/bank/bank.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'bank',
-    templateUrl: './app/bank/bank.component.html',
-   // styleUrls: ['/src/css/sub/bootstrapdark.css']
-})
-
-export class BankComponent {}
